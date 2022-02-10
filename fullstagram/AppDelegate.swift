477c7bef6c5579f4871f9c2c4ffcb473d3f8eb43
minkpang <pmink210@gmail.com>
2022-02-10 12:20:12 +0900

    add doubletap2like

---
+++ fullstagram/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  flitto_ios
+//
+//  Created by minkpang on 2022/01/27.
+//
+
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Thread.sleep(forTimeInterval: 2)
+        return true
+    }
+}
---
+++ fullstagram/fullstagramApp.swift
@@ -0,0 +1,18 @@
+//
+//  flitto_iosApp.swift
+//  flitto_ios
+//
+//  Created by minkpang on 2022/01/24.
+//
+
+import SwiftUI
+
+@main
+struct fullstagramApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
